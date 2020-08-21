9fbe35afdc09a970b7cd0caacb179718eeb3d766
Warren Maunier <64775563+Belios@users.noreply.github.com>
2020-08-21 15:42:40 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="main.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   </head>
 
   <header>
@@ -15,6 +16,12 @@
          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" style="color: #1A1A1A; left:80px;">&times;</a>
       </div>
       <ul class="sidebar-navigation">
+        <div class="search-container">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Rechercher.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
         <li class="header">Navigation</li>
         <li>
           <a href="index.php">
@@ -53,19 +60,15 @@
   </header>
   <body>
 
-
-
     <div class="hero">
       <div class="overlay">
         <div id="main">
           <button class="openbtn" onclick="openNav()">&#9776; </button>
         </div>
       </div>
-        <figure>
           <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
             <source src="media/wildlife.mp4" type="video/mp4">
             </video>
-        </figure>
       <div class="container h-100">
         <div class="d-flex h-100 text-center align-items-center">
           <div class="w-100 text-white" style="margin-bottom: 85vh;">
@@ -77,29 +80,31 @@
       </div>
     </div>
 
-    <section id="history" > <!-- Page history -->
-      <div class="container">
+    <section class="ong" > <!-- Page ong -->
+      <div class="container-">
+        <!-- <span class="border-animation" >
+          <div class="border-animation__inner"> -->
         <div class="row">
             <div class="col-sm-12 col-md-6 mb-4">
-            <img src="media/21.jpg">
+            <!-- <img class="scrollout" src="media/21.jpg"> -->
             </div>
 
-
             <div class="col-sm-12 col-md-6">
-              <div class=" border-5 border-left border-dark mb-5 pl-4"> <h3> SOUTENONS LA CAUSE ANIMALE </h3> </div>
+              <div class=" border-5 border-left border-dark mb-5 pl-4"> <h3 class="scrollout"> SOUTENONS LA CAUSE ANIMALE </h3> </div>
               <div>
-              <p class="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. <br><br>
+              <p class=" scrollout">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. <br><br>
               The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' <p>
               </div>
 
-              <button type="button" class="btn btn-danger mt-3 text-dark">En savoir plus</button>
+              <button type="button" class="btn btn-danger mt-3 text-dark scrollout">En savoir plus</button>
             </div>
           </div>
+    <!-- </div></span> -->
         </div>
-    </section> <!-- Fin History -->
+    </section> <!-- Fin ong -->
 
 
-
+<section>
   <div class="section-2">
     <div class="title">
       <div class="content">
@@ -124,8 +129,48 @@
     			<div class="box">
     				<img src="media/lion.jpg">
     	</div>
+      <div class="box">
+        <img src="media/bears.jpeg">
+      </div>
+      <div class="box">
+        <img src="media/chat.jpg">
+      </div>
+      <div class="box">
+        <img src="media/tigre.jpg">
+      </div>
+      <div class="box">
+        <img src="media/panthere.jpg">
+      </div>
+      <div class="box">
+        <img src="media/lynx.jpg">
+      </div>
+      <div class="box">
+        <img src="media/leopard.jpg">
+      </div>
     </div>
   </div>
+</section>
+
+
+<section class="contact">
+<div class="formulaire">
+  <h4> Nous contacter </h4>
+  <form action="" method="get" class="form-example">
+    <div class="form">
+      <input type="text" name="name" id="name" placeholder="Veuillez indiquer votre nom" style="width: 512px;" required>
+    </div>
+    <div class="form">
+      <input type="email" name="email" id="email" placeholder="Veuillez indiquer votre email" style="width: 512px;" required>
+    </div>
+    <div class="form">
+      <input type="text" name="message" id="message" placeholder="Veuillez indiquer votre message" style=" width: 512px; height:256px;" required>
+    </div>
+    <div class="form-button">
+      <input type="submit" value="Envoyer">
+    </div>
+  </form>
+</div>
+<section>
 
 
 
@@ -133,11 +178,9 @@
 
 
 
-
-
-
-
-
+<script> ScrollReveal().reveal('.scrollout', { delay: 2000 }); </script>
+<script src="scrollout.js"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
 <script src="index.js"></script>
 </body>
 
