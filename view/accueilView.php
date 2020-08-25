a08d0e9aed06356ab3d6f72017c841f9e536824e
Alexa VERMENOT <alexa.vermenot@orange.fr>
2020-08-25 16:13:24 +0200

    modif warren

--- view/accueilView.php
+++ view/accueilView.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   </head>
+
   <header>
     <div id="sidebar-container">
       <div class="sidebar-logo">
@@ -29,7 +30,7 @@
           </a>
         </li>
         <li>
-          <a href="view/connexionView.php">
+          <a href="#">
             <i class="fa fa-tachometer" aria-hidden="true"></i> Admin
           </a>
         </li>
@@ -64,7 +65,7 @@
           <button class="openbtn" onclick="openNav()">&#9776; </button>
         </div>
       </div>
-          <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+          <video class="parallax" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
             <source src="media/wildlife.mp4" type="video/mp4">
             </video>
       <div class="container h-100">
@@ -77,7 +78,7 @@
         </div>
       </div>
     </div>
-    <section class="ong" > <!-- Page ong -->
+    <section class="ong parallax" > <!-- Page ong -->
       <div class="container-">
         <!-- <span class="border-animation" >
           <div class="border-animation__inner"> -->
@@ -86,7 +87,7 @@
             <!-- <img class="scrollout" src="media/21.jpg"> -->
             </div>
             <div class="col-sm-12 col-md-6">
-              <div class=" border-5 border-left border-dark mb-5 pl-4"> <h3 class="scrollout"> SOUTENONS LA CAUSE ANIMALE </h3> </div>
+              <div class="border-5 border-left border-dark mb-5"> <h3 class="scrollout"> SOUTENONS LA CAUSE ANIMALE </h3> </div>
               <div>
               <p class=" scrollout">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. <br><br>
               The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' <p>
@@ -96,76 +97,66 @@
           </div>
     <!-- </div></span> -->
         </div>
-    </section>
-    <!-- Fin ong -->
-<!-- <section class="background-2">
-  <div class="section-2">
-    <span class="banner">
-    <div class="title">
-      <div class="content">
-        <h1> Catégories</h1>
-        <p> Retrouvez ici l'animal que vous souhaitez selon son cadre géographique, son espèce, son régime alimentaire ou tout simplement par son nom. </p>
-      </div>
-    </div>
-  </span> -->
+    </section> <!-- Fin ong -->
+
 <div class="section-2">
-  <div class="wrapper">
-        <!-- <h1 class="title" data-text="Bestiaire">Bestiaire</h1> -->
-      <div class="banniere">
-        <ul class="text hidden">
-          <li>B</li>
-          <li>E</li>
-          <li>S</li>
-          <li>T</li>
-          <li>I</li>
-          <li>A</li>
-          <li>I</li>
-          <li>R</li>
-          <li>E</li>
-           <li>&nbsp;</li>
-           <li>:</li>
-          <li class="spaced"> &nbsp; </li>
-          <li class="ghost">R</li>
-          <li class="ghost">e</li>
-          <li class="ghost">t</li>
-          <li class="ghost">r</li>
-          <li class="ghost">o</li>
-          <li class="ghost">u</li>
-          <li class="ghost">v</li>
-          <li class="ghost">e</li>
-          <li class="ghost">r</li>
-          <li class="ghost">&nbsp;</li>
-          <li class="ghost">l</li>
-          <li class="ghost">a</li>
-          <li class="ghost">&nbsp;</li>
-          <li class="ghost">l</li>
-          <li class="ghost">i</li>
-            <li class="ghost">s</li>
-          <li class="ghost">t</li>
-          <li class="ghost">e</li>
-          <li class="ghost">&nbsp;</li>
-          <li class="ghost">d</li>
-            <li class="ghost">e</li>
-          <li class="ghost">&nbsp;</li>
-          <li class="ghost">t</li>
-          <li class="ghost">o</li>
-          <li class="ghost">u</li>
-            <li class="ghost">t</li>
-          <li class="ghost">&nbsp;</li>
-          <li class="ghost">l</li>
-          <li class="ghost">e</li>
+    <div class="wrapper parallax">
+      <!-- <h1 class="title" data-text="Bestiaire">Bestiaire</h1> -->
+    <div class="banniere">
+      <ul class="text hidden">
+        <li>B</li>
+        <li>E</li>
+        <li>S</li>
+        <li>T</li>
+        <li>I</li>
+        <li>A</li>
+        <li>I</li>
+        <li>R</li>
+        <li>E</li>
+         <li>&nbsp;</li>
+         <li>:</li>
+        <li class="spaced"> &nbsp; </li>
+        <li class="ghost">R</li>
+        <li class="ghost">e</li>
+        <li class="ghost">t</li>
+        <li class="ghost">r</li>
+        <li class="ghost">o</li>
+        <li class="ghost">u</li>
+        <li class="ghost">v</li>
+        <li class="ghost">e</li>
+        <li class="ghost">r</li>
+        <li class="ghost">&nbsp;</li>
+        <li class="ghost">l</li>
+        <li class="ghost">a</li>
+        <li class="ghost">&nbsp;</li>
+        <li class="ghost">l</li>
+        <li class="ghost">i</li>
           <li class="ghost">s</li>
-            <li class="ghost">&nbsp;</li>
-          <li class="ghost">a</li>
-          <li class="ghost">n</li>
-          <li class="ghost">i</li>
-          <li class="ghost">m</li>
-            <li class="ghost">a</li>
-          <li class="ghost">u</li>
-          <li class="ghost">x</li>
+        <li class="ghost">t</li>
+        <li class="ghost">e</li>
+        <li class="ghost">&nbsp;</li>
+        <li class="ghost">d</li>
+          <li class="ghost">e</li>
+        <li class="ghost">&nbsp;</li>
+        <li class="ghost">t</li>
+        <li class="ghost">o</li>
+        <li class="ghost">u</li>
+          <li class="ghost">t</li>
+        <li class="ghost">&nbsp;</li>
+        <li class="ghost">l</li>
+        <li class="ghost">e</li>
+        <li class="ghost">s</li>
           <li class="ghost">&nbsp;</li>
-        </ul>
-      </div>
+        <li class="ghost">a</li>
+        <li class="ghost">n</li>
+        <li class="ghost">i</li>
+        <li class="ghost">m</li>
+          <li class="ghost">a</li>
+        <li class="ghost">u</li>
+        <li class="ghost">x</li>
+        <li class="ghost">&nbsp;</li>
+      </ul>
+    </div>
       <div class="container">
         <div class='items'>
           <?php foreach ($animals as $animal) {
@@ -191,105 +182,15 @@
                </svg></a>
              </div>
         <?php   } ?>
-
-             <!-- <div class='item'>
-               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
-                 <defs>
-                   <clipPath id='clip-1'>
-                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
-                   </clipPath>
-                 </defs>
-                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
-                   Léopard
-                 </text>
-                 <g clip-path='url(#clip-1)'>
-                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/leopard.jpg'></image>
-                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
-                     Léopard
-                   </text>
-                 </g>
-               </svg>
-             </div>
-             <div class='item'>
-               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
-                 <defs>
-                   <clipPath id='clip-2'>
-                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
-                   </clipPath>
-                 </defs>
-                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
-                   Lion
-                 </text>
-                 <g clip-path='url(#clip-2)'>
-                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/lion.jpg'></image>
-                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
-                     Lion
-                   </text>
-                 </g>
-               </svg>
-             </div>
-             <div class='item'>
-               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
-                 <defs>
-                   <clipPath id='clip-3'>
-                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
-                   </clipPath>
-                 </defs>
-                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
-                   Panthere
-                 </text>
-                 <g clip-path='url(#clip-3)'>
-                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/panthere.jpg'></image>
-                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
-                    Panthere
-                   </text>
-                 </g>
-               </svg>
-             </div>
-             <div class='item'>
-               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
-                 <defs>
-                   <clipPath id='clip-4'>
-                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
-                   </clipPath>
-                 </defs>
-                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
-                   Tortue
-                 </text>
-                 <g clip-path='url(#clip-4)'>
-                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/turtle.jpg'></image>
-                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
-                    Tortue
-                   </text>
-                 </g>
-               </svg>
-             </div>
-             <div class='item'>
-               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
-                 <defs>
-                   <clipPath id='clip-5'>
-                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
-                   </clipPath>
-                 </defs>
-                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
-                   Loup blanc
-                 </text>
-                 <g clip-path='url(#clip-5)'>
-                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/whitewolf.jpg'></image>
-                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
-                    Loup blanc
-                   </text>
-                 </g>
-               </svg>
-             </div> -->
     </div>
   </div>
 </div>
+  
 <section class="section-3">
-  <div class="formulaire">
+  <div class="formulaire parallax">
   <div class="contact-box contact">
     <h2>Nous contacter</h2>
-    <form action="">
+    <form>
       <div class="user-box">
         <input type="text" name="name" id="name" required="">
         <label>Nom</label>
@@ -302,13 +203,13 @@
         <input type="text" name="message" id="message" required="" style="height:256px;">
         <label>Message</label>
       </div>
-      <a href="#" onClick="formulaire()">
+      <a href="#">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-        Submit
-    </a>
+        Envoyer
+      </a>
     </form>
   </div>
 </div>
@@ -317,49 +218,9 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script src="scrollout.js"></script>
 <script src="https://unpkg.com/scrollreveal"></script>
 <script src="index.js"></script>
-<script src="contact.js"></script>
-<script> var items = []
-  , point = document.querySelector('svg').createSVGPoint();
-function getCoordinates(e, svg) {
-  point.x = e.clientX;
-  point.y = e.clientY;
-  return point.matrixTransform(svg.getScreenCTM().inverse());
-}
-function changeColor(e) {
-  document.body.className = e.currentTarget.className;
-}
-function Item(config) {
-  Object.keys(config).forEach(function (item) {
-    this[item] = config[item];
-  }, this);
-  this.el.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
-  this.el.addEventListener('touchmove', this.touchMoveHandler.bind(this));
-}
-Item.prototype = {
-  update: function update(c) {
-    this.clip.setAttribute('cx', c.x);
-    this.clip.setAttribute('cy', c.y);
-  },
-  mouseMoveHandler: function mouseMoveHandler(e) {
-    this.update(getCoordinates(e, this.svg));
-  },
-  touchMoveHandler: function touchMoveHandler(e) {
-    e.preventDefault();
-    var touch = e.targetTouches[0];
-    if (touch) return this.update(getCoordinates(touch, this.svg));
-  }
-};
-[].slice.call(document.querySelectorAll('.item'), 0).forEach(function (item, index) {
-  items.push(new Item({
-    el: item,
-    svg: item.querySelector('svg'),
-    clip: document.querySelector('#clip-'+index+' circle'),
-  }));
-});
-[].slice.call(document.querySelectorAll('button'), 0).forEach(function (button) {
-  button.addEventListener('click', changeColor);
-}); </script>
 </body>
 </html>
