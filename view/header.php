16766e3c5c854e7301bc4002854a0aa4b2bdaffe
Alexa VERMENOT <alexa.vermenot@orange.fr>
2020-08-25 16:30:28 +0200

    Update header.php

--- view/header.php
+++ view/header.php
@@ -14,7 +14,7 @@ if (isset($_SESSION)=== false) {
 		<meta charset="UTF-8" />
     	<meta name="viewport" content="width=device-width, initial-scale=1">
     	<link rel="stylesheet" href="styleConnexionView.css" />
-    	<link rel="stylesheet" href="styleAdminView.css" />
+    	<link rel="stylesheet" href="view/styleAdminView.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
