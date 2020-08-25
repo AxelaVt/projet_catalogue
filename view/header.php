3c8eba072eccaf2425bcb0e6adc4bf1b89ad546d
Alexa VERMENOT <alexa.vermenot@orange.fr>
2020-08-25 13:41:46 +0200

    modif css link

--- view/header.php
+++ view/header.php
@@ -13,8 +13,8 @@ if (isset($_SESSION)=== false) {
 	<head>
 		<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/styleConnexionView.css" />
-    <link rel="stylesheet" href="view/css/styleAdminView.css" />
+    <link rel="stylesheet" href="styleConnexionView.css" />
+    <link rel="stylesheet" href="view/styleAdminView.css" />
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
