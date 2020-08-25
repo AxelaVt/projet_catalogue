bb4a70d712cd77240218faa646e46bf611e4da39
Alexa VERMENOT <alexa.vermenot@orange.fr>
2020-08-25 14:11:48 +0200

    modif links javascript files

--- view/accueilView.php
+++ view/accueilView.php
@@ -318,8 +318,8 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/scrollreveal"></script>
-<script src="js/index.js"></script>
-<script src="js/contact.js"></script>
+<script src="../index.js"></script>
+<script src="../contact.js"></script>
 <script> var items = []
   , point = document.querySelector('svg').createSVGPoint();
 function getCoordinates(e, svg) {
