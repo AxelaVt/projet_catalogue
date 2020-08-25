99d4cb126730aa12ed1184958475735aaa3a986c
Alexa VERMENOT <alexa.vermenot@orange.fr>
2020-08-25 15:31:10 +0200

    add button

--- view/connexionView.php
+++ view/connexionView.php
@@ -37,7 +37,8 @@ include_once ('header.php');
                             ?>
                           </div>
                           <div class="col-lg-6 login-btm login-button float-sm-right">
-                              <button type="submit" class="btn btn-outline-primary">LOGIN</button>
+                              <button type="submit" class="btn btn-outline-primary my-1">LOGIN</button>
+                              <button type="button" value="Cancel" onclick="history.back()" class="btn btn-outline-primary my-1">Cancel</button>
                           </div>
                       </div>
                   </form>
@@ -46,33 +47,3 @@ include_once ('header.php');
           <div class="col-lg-3 col-md-2"></div>
       </div>
   </div>
-
-  <!-- <div id="container" class="container h-100 m-0">
-      <!-- form connexion -->
-    <!-- <div class="d-flex col-lg-8 col-sm-12 align-self-center">
-      <form class="w-100 p-5" action="../session.php" method="POST">
-          <h1 class="align-self-center mb-4">Connexion</h1>
-          <div class="form-group">
-          <label><b>Nom d'utilisateur</b></label>
-          <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-          </div>
-          <div class="form-group">
-          <label><b>Mot de passe</b></label>
-          <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-          </div>
-          <input type="submit" id='submit' value='LOGIN' >
-      </form>
-    </div> -->
-
-  <!-- </div> -->
-  <!-- <?php
-  // if(isset($_GET['erreur'])){
-  //     $err = $_GET['erreur'];
-  //     if($err==1 || $err==2){
-  //         echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-  // }
-  //  }
-  ?> -->
-
-  <!-- header('Location: blog.php'); //Redirection immédiate
-  header("Refresh: 3;URL=blog.php"); //Redirection avec délai (en secondes) -->
