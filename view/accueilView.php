8386cc358e8ff9a7cd5b0c9bac6bbd23193219ca
Alexa VERMENOT <alexa.vermenot@orange.fr>
2020-08-25 10:05:12 +0200

    modif page users

--- view/accueilView.php
+++ view/accueilView.php
@@ -1,17 +1,365 @@
-<?php
-include "header.php";
 
 
-foreach ($animals as $animal){
-  echo '<strong>'. $animal['name'] .'</strong><br />';
-  echo '<strong>'. $animal['id'] .'</strong><br />';
-  echo '<strong>'. $animal['description'] .'</strong><br />';
-  ?>
-  <img src="get_image.php?id=<?= $animal['id'] ?>" alt="<?=$animal['name']?>">
-  <?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="view/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  </head>
+  <header>
+    <div id="sidebar-container">
+      <div class="sidebar-logo">
+        Projet Bestiaire
+         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" style="color: #1A1A1A; left:80px;">&times;</a>
+      </div>
+      <ul class="sidebar-navigation">
+        <div class="search-container">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Rechercher.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
+        <li class="header">Navigation</li>
+        <li>
+          <a href="index.php">
+            <i class="fa fa-home" aria-hidden="true"></i> Accueil
+          </a>
+        </li>
+        <li>
+          <a href="view/connexionView.php">
+            <i class="fa fa-tachometer" aria-hidden="true"></i> Admin
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-tachometer" aria-hidden="true"></i> FAQ
+          </a>
+        </li>
+        <li class="header">Catégories</li>
+        <li>
+          <a href="#">
+            <i class="fa fa-users" aria-hidden="true"></i> Alimentation
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-cog" aria-hidden="true"></i> Zone Géographique
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-info-circle" aria-hidden="true"></i> Famille
+          </a>
+        </li>
+      </ul>
+    </div>
+  </header>
+  <body>
+    <div class="hero">
+      <div class="overlay">
+        <div id="main">
+          <button class="openbtn" onclick="openNav()">&#9776; </button>
+        </div>
+      </div>
+          <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+            <source src="media/wildlife.mp4" type="video/mp4">
+            </video>
+      <div class="container h-100">
+        <div class="d-flex h-100 text-center align-items-center">
+          <div class="w-100 text-white" style="margin-bottom: 85vh;">
+            <img src="icons/logo1.png">
+            <h1 class="display-3">Projet Bestiaire</h1>
+            <p class="lead mb-0">Ceci est la vidéo d'accueil du projet bestiaire. Ce site a pour but de donner une description simple et claire sur la plupart des animaux qui vivent sur la planète.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="ong" > <!-- Page ong -->
+      <div class="container-">
+        <!-- <span class="border-animation" >
+          <div class="border-animation__inner"> -->
+        <div class="row">
+            <div class="col-sm-12 col-md-6 mb-4">
+            <!-- <img class="scrollout" src="media/21.jpg"> -->
+            </div>
+            <div class="col-sm-12 col-md-6">
+              <div class=" border-5 border-left border-dark mb-5 pl-4"> <h3 class="scrollout"> SOUTENONS LA CAUSE ANIMALE </h3> </div>
+              <div>
+              <p class=" scrollout">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. <br><br>
+              The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' <p>
+              </div>
+              <button type="button" class="btn btn-danger mt-3 text-dark scrollout">En savoir plus</button>
+            </div>
+          </div>
+    <!-- </div></span> -->
+        </div>
+    </section>
+    <!-- Fin ong -->
+<!-- <section class="background-2">
+  <div class="section-2">
+    <span class="banner">
+    <div class="title">
+      <div class="content">
+        <h1> Catégories</h1>
+        <p> Retrouvez ici l'animal que vous souhaitez selon son cadre géographique, son espèce, son régime alimentaire ou tout simplement par son nom. </p>
+      </div>
+    </div>
+  </span> -->
+<div class="section-2">
+  <div class="wrapper">
+        <!-- <h1 class="title" data-text="Bestiaire">Bestiaire</h1> -->
+      <div class="banniere">
+        <ul class="text hidden">
+          <li>B</li>
+          <li>E</li>
+          <li>S</li>
+          <li>T</li>
+          <li>I</li>
+          <li>A</li>
+          <li>I</li>
+          <li>R</li>
+          <li>E</li>
+           <li>&nbsp;</li>
+           <li>:</li>
+          <li class="spaced"> &nbsp; </li>
+          <li class="ghost">R</li>
+          <li class="ghost">e</li>
+          <li class="ghost">t</li>
+          <li class="ghost">r</li>
+          <li class="ghost">o</li>
+          <li class="ghost">u</li>
+          <li class="ghost">v</li>
+          <li class="ghost">e</li>
+          <li class="ghost">r</li>
+          <li class="ghost">&nbsp;</li>
+          <li class="ghost">l</li>
+          <li class="ghost">a</li>
+          <li class="ghost">&nbsp;</li>
+          <li class="ghost">l</li>
+          <li class="ghost">i</li>
+            <li class="ghost">s</li>
+          <li class="ghost">t</li>
+          <li class="ghost">e</li>
+          <li class="ghost">&nbsp;</li>
+          <li class="ghost">d</li>
+            <li class="ghost">e</li>
+          <li class="ghost">&nbsp;</li>
+          <li class="ghost">t</li>
+          <li class="ghost">o</li>
+          <li class="ghost">u</li>
+            <li class="ghost">t</li>
+          <li class="ghost">&nbsp;</li>
+          <li class="ghost">l</li>
+          <li class="ghost">e</li>
+          <li class="ghost">s</li>
+            <li class="ghost">&nbsp;</li>
+          <li class="ghost">a</li>
+          <li class="ghost">n</li>
+          <li class="ghost">i</li>
+          <li class="ghost">m</li>
+            <li class="ghost">a</li>
+          <li class="ghost">u</li>
+          <li class="ghost">x</li>
+          <li class="ghost">&nbsp;</li>
+        </ul>
+      </div>
+      <div class="container">
+        <div class='items'>
+          <?php foreach ($animals as $animal) {
+
+           ?>
+             <div class='item'>
+               <a href="animal.php?id=<?= $animal['id'] ?>">
+               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
+                 <defs>
+                   <clipPath id='clip-0'>
+                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
+                   </clipPath>
+                 </defs>
+                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
+
+                 </text>
+                 <g clip-path='url(#clip-0)'>
+                 <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href="get_image.php?id=<?= $animal['id'] ?>"</image>
+                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
+                    <?php echo $animal['name'] ?>
+                   </text>
+                 </g>
+               </svg></a>
+             </div>
+        <?php   } ?>
+
+             <!-- <div class='item'>
+               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
+                 <defs>
+                   <clipPath id='clip-1'>
+                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
+                   </clipPath>
+                 </defs>
+                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
+                   Léopard
+                 </text>
+                 <g clip-path='url(#clip-1)'>
+                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/leopard.jpg'></image>
+                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
+                     Léopard
+                   </text>
+                 </g>
+               </svg>
+             </div>
+             <div class='item'>
+               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
+                 <defs>
+                   <clipPath id='clip-2'>
+                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
+                   </clipPath>
+                 </defs>
+                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
+                   Lion
+                 </text>
+                 <g clip-path='url(#clip-2)'>
+                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/lion.jpg'></image>
+                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
+                     Lion
+                   </text>
+                 </g>
+               </svg>
+             </div>
+             <div class='item'>
+               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
+                 <defs>
+                   <clipPath id='clip-3'>
+                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
+                   </clipPath>
+                 </defs>
+                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
+                   Panthere
+                 </text>
+                 <g clip-path='url(#clip-3)'>
+                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/panthere.jpg'></image>
+                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
+                    Panthere
+                   </text>
+                 </g>
+               </svg>
+             </div>
+             <div class='item'>
+               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
+                 <defs>
+                   <clipPath id='clip-4'>
+                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
+                   </clipPath>
+                 </defs>
+                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
+                   Tortue
+                 </text>
+                 <g clip-path='url(#clip-4)'>
+                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/turtle.jpg'></image>
+                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
+                    Tortue
+                   </text>
+                 </g>
+               </svg>
+             </div>
+             <div class='item'>
+               <svg preserveAspectRatio='xMidYMid slice' viewBox='0 0 300 200'>
+                 <defs>
+                   <clipPath id='clip-5'>
+                     <circle cx='0' cy='0' fill='#000' r='150px'></circle>
+                   </clipPath>
+                 </defs>
+                 <text class='svg-text' dy='.3em' x='50%' y='50%'>
+                   Loup blanc
+                 </text>
+                 <g clip-path='url(#clip-5)'>
+                   <image height='100%' preserveAspectRatio='xMinYMin slice' width='100%' xlink:href='media/whitewolf.jpg'></image>
+                   <text class='svg-masked-text' dy='.3em' x='50%' y='50%'>
+                    Loup blanc
+                   </text>
+                 </g>
+               </svg>
+             </div> -->
+    </div>
+  </div>
+</div>
+<section class="section-3">
+  <div class="formulaire">
+  <div class="contact-box contact">
+    <h2>Nous contacter</h2>
+    <form action="">
+      <div class="user-box">
+        <input type="text" name="name" id="name" required="">
+        <label>Nom</label>
+      </div>
+      <div class="user-box">
+        <input type="email" name="" id="email" required="">
+        <label>E-mail</label>
+      </div>
+          <div class="user-box">
+        <input type="text" name="message" id="message" required="" style="height:256px;">
+        <label>Message</label>
+      </div>
+      <a href="#" onClick="formulaire()">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Submit
+    </a>
+    </form>
+  </div>
+</div>
+<section>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
+<script src="js/index.js"></script>
+<script src="js/contact.js"></script>
+<script> var items = []
+  , point = document.querySelector('svg').createSVGPoint();
+function getCoordinates(e, svg) {
+  point.x = e.clientX;
+  point.y = e.clientY;
+  return point.matrixTransform(svg.getScreenCTM().inverse());
 }
-
-require 'footer.php';
-?>
-
-<a class="float-sm-right p-2" href="view/connexionView.php"><img src="bootstrap-icons/key-fill.svg" alt="connection" width="32" height="32" title="Bootstrap"></a>
+function changeColor(e) {
+  document.body.className = e.currentTarget.className;
+}
+function Item(config) {
+  Object.keys(config).forEach(function (item) {
+    this[item] = config[item];
+  }, this);
+  this.el.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
+  this.el.addEventListener('touchmove', this.touchMoveHandler.bind(this));
+}
+Item.prototype = {
+  update: function update(c) {
+    this.clip.setAttribute('cx', c.x);
+    this.clip.setAttribute('cy', c.y);
+  },
+  mouseMoveHandler: function mouseMoveHandler(e) {
+    this.update(getCoordinates(e, this.svg));
+  },
+  touchMoveHandler: function touchMoveHandler(e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) return this.update(getCoordinates(touch, this.svg));
+  }
+};
+[].slice.call(document.querySelectorAll('.item'), 0).forEach(function (item, index) {
+  items.push(new Item({
+    el: item,
+    svg: item.querySelector('svg'),
+    clip: document.querySelector('#clip-'+index+' circle'),
+  }));
+});
+[].slice.call(document.querySelectorAll('button'), 0).forEach(function (button) {
+  button.addEventListener('click', changeColor);
+}); </script>
+</body>
+</html>
--- view/adminView.php
+++ view/adminView.php
@@ -6,13 +6,13 @@ include "header.php";
 <div class="container">
   <div class="container d-flex flex-column my-5">
     <div id="mySidenav" class="sidenav">
-      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNavAdmin()">&times;</a>
 
       <!-- <a href="index.php">Accueil du site</a> -->
       <a href="admin.php?deconnexion=true">Quitter</a>
       <a href="view/passwordView.php">Changer mot de passe</a>
     </div>
-    <span style="font-size:1.5rem;cursor:pointer" onclick="openNav()"> &#9776; <?php echo "Bonjour $user, ";?></span>
+    <span style="font-size:1.5rem;cursor:pointer" onclick="openNavAdmin()"> &#9776; <?php echo "Bonjour $user, ";?></span>
   </div>
 
   <div class="d-flex flex-row justify-content-center mb-5">
@@ -33,16 +33,16 @@ include "header.php";
         <div class="row h-10 m-3">
         <a href='view/addView.php' type="button" class="btn btn-outline-secondary btn-lg float-sm-center p-2">Ajouter</a>
         </div>
-        <table class="table table-striped w-100 h-auto">
+        <table class="table table-striped w-100 h-auto" data-toggle="table" data-search="true" data-pagination="true">
           <thead>
             <tr>
-              <th>id</th>
-              <th>animal</th>
-              <th>type</th>
-              <th>famille</th>
+              <th data-sortable="true" data-field="id">id</th>
+              <th data-sortable="true" data-field="animal">animal</th>
+              <th data-sortable="true" data-field="type">type</th>
+              <th data-sortable="true" data-field="famille">famille</th>
               <th>Modification</th>
               <th>supprimer</th>
-              <th>actif/archiver</th>
+              <th data-sortable="true" data-field="actif">actif/archiver</th>
             </tr>
           </thead>
           <tbody>
@@ -51,10 +51,10 @@ include "header.php";
 
             ?>
             <tr>
-              <td><?= $animal['id'] ?></td>
-              <td><?= $animal['name'] ?></td>
-              <td><?= $animal['type'] ?></td>
-              <td><?= $animal['family'] ?></td>
+              <td><?= htmlspecialchars($animal['id']) ?></td>
+              <td><?= htmlspecialchars($animal['name']) ?></td>
+              <td><?= htmlspecialchars($animal['type']) ?></td>
+              <td><?= htmlspecialchars($animal['family']) ?></td>
               <td class="text-center"><a href="change.php?id=<?= $animal['id'] ?>" ><img src="view/bootstrap-icons/pencil-square.svg" alt="change" width="16" height="16" title="change"></a></td>
               <td class="text-center"><a href="delete.php?id=<?= $animal['id'] ?>" ><img src="view/bootstrap-icons/trash.svg" alt="delete" width="16" height="16" title="delete"></a></td>
               <td class="text-center">
@@ -90,31 +90,28 @@ include "header.php";
           <tbody>
 
               <?php
-              foreach ($users as $user){
+              //var_dump($adms);
+              foreach ($adms as $adm){
                 ?>
               <tr>
-                <td><?= $user['id'] ?></td>
-                <td><?= $user['username'] ?></td>
-                <td class="text-center"><a href="passwordView.php" ><img src="view/bootstrap-icons/key.svg" alt="change" width="16" height="16" title="change"></a></td>
-
+                <td><?= htmlspecialchars($adm['id']) ?></td>
+                <td><?= htmlspecialchars($adm['username']) ?></td>
+                <td class="text-center"><a href="view/passwordView.php" ><img src="view/bootstrap-icons/key.svg" alt="change" width="16" height="16" title="change"></a></td>
               </tr>
                 <?php
               }?>
-
           </tbody>
         </table>
-
       </div>
-
     </div>
 </div>
 </body>
 <script type="text/javascript">
-function openNav() {
+function openNavAdmin() {
   document.getElementById("mySidenav").style.width = "250px";
 }
 
-function closeNav() {
+function closeNavAdmin() {
   document.getElementById("mySidenav").style.width = "0";
 }
 </script>
